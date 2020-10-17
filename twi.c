12d3b8a6f7378b1e242c16f4947e180a9033ed9d
Olaf Rempel <razzor@kopf-tisch.de>
2020-10-17 22:20:05 +0200

    twiboot: convert indentation to spaces

--- twi.c
+++ twi.c
@@ -42,27 +42,27 @@
 
 #define TWI_DEFAULT_DEVICE  "/dev/i2c-0"
 
-#define READ_BLOCK_SIZE		128	/* bytes in one flash/eeprom read request */
-#define WRITE_BLOCK_SIZE	 16	/* bytes in one eeprom write request */
+#define READ_BLOCK_SIZE         128     /* bytes in one flash/eeprom read request */
+#define WRITE_BLOCK_SIZE        16      /* bytes in one eeprom write request */
 
 /* SLA+R */
-#define CMD_WAIT		0x00
-#define CMD_READ_VERSION	0x01
-#define CMD_READ_MEMORY		0x02
+#define CMD_WAIT                0x00
+#define CMD_READ_VERSION        0x01
+#define CMD_READ_MEMORY         0x02
 
 /* SLA+W */
-#define CMD_SWITCH_APPLICATION	CMD_READ_VERSION
-#define CMD_WRITE_MEMORY	CMD_READ_MEMORY
+#define CMD_SWITCH_APPLICATION  CMD_READ_VERSION
+#define CMD_WRITE_MEMORY        CMD_READ_MEMORY
 
 /* CMD_SWITCH_APPLICATION parameter */
-#define BOOTTYPE_BOOTLOADER	0x00				/* only in APP */
-#define BOOTTYPE_APPLICATION	0x80
+#define BOOTTYPE_BOOTLOADER     0x00    /* only in APP */
+#define BOOTTYPE_APPLICATION    0x80
 
 /* CMD_{READ|WRITE}_* parameter */
-#define MEMTYPE_CHIPINFO	0x00
-#define MEMTYPE_FLASH		0x01
-#define MEMTYPE_EEPROM		0x02
-#define MEMTYPE_PARAMETERS	0x03				/* only in APP */
+#define MEMTYPE_CHIPINFO        0x00
+#define MEMTYPE_FLASH           0x01
+#define MEMTYPE_EEPROM          0x02
+#define MEMTYPE_PARAMETERS      0x03    /* only in APP */
 
 struct multiboot_ops twi_ops;
 
